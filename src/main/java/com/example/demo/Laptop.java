6408f1aeaf8985f84dcff37d73b15c77f4d1a155
ramuninagendra <ramuninaagendra00@gmail.com>
2021-07-23 12:15:51 +0530

    second chenges

---
+++ src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	public Laptop() {
+
+		System.out.println("laptop object was created");
+		
+		
+	}
+
+}
