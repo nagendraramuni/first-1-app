eb0cd8a1b4c126282fa7bed6bd583ba2608f9a53
ramuninagendra <ramuninaagendra00@gmail.com>
2021-07-23 12:21:45 +0530

    third chenges

---
+++ src/main/java/com/example/demo/Mouse.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Repository
+@Component
+public class Mouse {
+	
+	 public Mouse() {
+	
+		 
+		System.out.println("mouse object was created"); 
+		 
+		 
+		 
+		 
+		
+	}
+
+}
